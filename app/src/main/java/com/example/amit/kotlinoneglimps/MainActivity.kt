389e21693e3dac94c053dbc8a197c6822202fa93
unknown <souravsonirjit5@gmail.com>
2019-07-13 23:43:57 +0530

    Initial commit

---
+++ app/src/main/java/com/example/amit/kotlinoneglimps/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.amit.kotlinoneglimps
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setTextValue()
+
+        }
+
+        fun setTextValue(value : Boolean=false): Boolean
+        {
+            var foodList= arrayListOf<String>("North Indian","South Indian","Dosa","Idaly","Samosa")
+
+            bt_choose_food.setOnClickListener {
+
+                var random=Random();
+                var randomFood=random.nextInt(foodList.count())
+                tv_hello.text=foodList[randomFood];
+
+
+            }
+            return true
+    }
+}
